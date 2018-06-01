24b9cc4099e12acf8c420ce30daf2748978cfefe
Vince <vince.chanl26@gmail.com>
2018-06-01 11:54:48 +0800

    Add | dotenv global params

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,4 +1,9 @@
+require('dotenv').config({ silent: true })
+
 'use strict'
 module.exports = {
-  NODE_ENV: '"production"'
+  NODE_ENV: '"production"',
+  API_HOST: JSON.stringify(process.env.API_HOST),
+  DEFAULT_LANG: JSON.stringify(process.env.DEFAULT_LANG),
+  PORT: JSON.stringify(process.env.PORT),
 }
