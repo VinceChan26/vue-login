5bfeef73ad8675950735105f7c1733397c6f64d0
Vince Chan <vince.chanl26@gmail.com>
2019-01-14 18:16:40 +0800

    Fix webpack-dev-server security and upgrade to webpack 4

--- README.md
+++ README.md
@@ -70,6 +70,6 @@ yarn install
 # serve with hot reload at localhost:8888
 yarn start
 
-# start json-server (啟動 JSON Server)
+# start json-server (啟動 JSON Server)
 json-server --watch db.json
 ```
--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -9,11 +9,13 @@ const CopyWebpackPlugin = require('copy-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const FriendlyErrorsPlugin = require('friendly-errors-webpack-plugin')
 const portfinder = require('portfinder')
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
 
 const HOST = process.env.HOST
 const PORT = process.env.PORT && Number(process.env.PORT)
 
 const devWebpackConfig = merge(baseWebpackConfig, {
+  mode: 'development',
   module: {
     rules: utils.styleLoaders({ sourceMap: config.dev.cssSourceMap, usePostCSS: true })
   },
@@ -49,8 +51,6 @@ const devWebpackConfig = merge(baseWebpackConfig, {
       'process.env': require('../config/dev.env')
     }),
     new webpack.HotModuleReplacementPlugin(),
-    new webpack.NamedModulesPlugin(), // HMR shows correct file names in console on update.
-    new webpack.NoEmitOnErrorsPlugin(),
     // https://github.com/ampedandwired/html-webpack-plugin
     new HtmlWebpackPlugin({
       filename: 'index.html',
@@ -64,7 +64,9 @@ const devWebpackConfig = merge(baseWebpackConfig, {
         to: config.dev.assetsSubDirectory,
         ignore: ['.*']
       }
-    ])
+    ]),
+    // Necesary configuration for vue-loader v15
+    new VueLoaderPlugin()
   ]
 })
 
